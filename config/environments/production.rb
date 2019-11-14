83affc7ed220c210e1d7723eb5b1a2e8cd2d497f
karlo <deguzmankarlo@gmail.com>
2019-11-14 13:47:07 +0000

    Add material ui to dependencies

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.assets.initialize_on_precompile = false
+  config.secret_key_base = 'YourSecretKeyHere'
 
   # Code is not reloaded between requests.
   config.cache_classes = true
